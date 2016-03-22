50f40399572eae9485d7f9cfb00e18996da98d92
phi3 <phi3@linux2.gl.umbc.edu>
2016-03-22 18:01:16 -0400

    setting up framework for video class

---
+++ Video.cpp
@@ -0,0 +1,10 @@
+#ifndef _VIDEO_CPP
+#define _VIDEO_CPP
+
+#include "Video.h"
+
+// class definition will go here
+
+
+
+#endif // protection
---
+++ Video.h
@@ -0,0 +1,19 @@
+#ifndef _VIDEO_H
+#define _VIDEO_H
+
+
+
+class Video {
+ public:
+  Video();
+
+
+
+ private:
+  m_title;
+
+
+};
+
+
+#endif // protection
